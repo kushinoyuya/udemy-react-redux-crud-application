e777142fb0355a259895e46cb3f23037b5aaa116
kushinoyuya <kusshy15059855@gmail.com>
2020-02-15 20:27:14 +0900

    jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello, world!</h1>
+//     </div>
+//   );
+// }
+
+// class App extends Component{
+//   render(){
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, world"
+//     );
+//   }
+// }
+
+class App extends Component{
+  render(){
+    return React.createElement(
+      "h1",
+      null,
+      "Hello, world!!!!!!"
+    )
+  }
 }
+
 
 export default App;

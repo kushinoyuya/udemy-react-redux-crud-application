0b6af02821e73b8dfe19a758235512cf4f299d9f
kushinoyuya <kusshy15059855@gmail.com>
2020-02-20 05:41:20 +0900

    more-jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-
-
-// class App extends Component {
-//   render(){
-//     return(
-//     <div>
-//       <label html For= "bar">bar</label>
-//       <input type= "text" onClick={() => {console.log("I am clicked")}}/ >
-//     </div>
-//     )
-//   }
-// }
+import React from 'react';
 
 const App = () => {
-  return <div>Hi </div>
+  const profiles = [
+    {name : "Taro", age: 10},
+    {name : "Hanako", age: 5},
+    {name : "NoName"}
+  ]
+  return(
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name= {profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
+  )
 }
 
-const Cat = () => {
-  return <div>Me </div>
+const User = (props) => {
+  return <div> Hi, I am {props.name}!, and {props.age} yaers old</div>
 }
 
-
+User.defaultProps = {
+  age:1
+}
 
 export default App;
